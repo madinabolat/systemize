e5ff72830a92bf396b69ca56ffe8d383abd67e3a
madinabolat <madinabolat01@gmail.com>
2025-09-26 08:44:03 -0400

    refactor

--- peach-academy/src/main/java/com/example/peachacademy/PeachAcademyApplication.java
+++ peach-academy/src/main/java/com/example/peachacademy/PeachAcademyApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.peachacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PeachAcademyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PeachAcademyApplication.class, args);
 	}
 
 }
