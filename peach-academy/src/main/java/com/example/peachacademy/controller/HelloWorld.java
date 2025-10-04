2c109614ee4971123dbd03078609585c9e7ffc2f
madinabolat <madinabolat01@gmail.com>
2025-10-03 22:25:07 -0400

    add comments

--- peach-academy/src/main/java/com/example/peachacademy/controller/HelloWorld.java
+++ peach-academy/src/main/java/com/example/peachacademy/controller/HelloWorld.java
@@ -3,12 +3,14 @@ package com.example.peachacademy.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//why @RestController? not just Controller?
 @RestController
 public class HelloWorld {
-
+    //? what is @RequestMapping
     @GetMapping("/hello")
     public String HelloWorld(){
         return "Hello World!!";
     }
-
+    //can also return html here
+    //the file will leave in resources?
 }
