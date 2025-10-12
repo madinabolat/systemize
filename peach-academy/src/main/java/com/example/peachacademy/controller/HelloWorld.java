9294e52e53269e5e825541ba620cbee73390e36a
madinabolat <madinabolat01@gmail.com>
2025-10-11 23:37:40 -0400

    Update HelloWorld.java

--- peach-academy/src/main/java/com/example/peachacademy/controller/HelloWorld.java
+++ peach-academy/src/main/java/com/example/peachacademy/controller/HelloWorld.java
@@ -5,14 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@Controller is used to render html views (Spring MVC apps)
-//@RestController is used for API
 public class HelloWorld {
     //? what is @RequestMapping
     @GetMapping("/hello")
     public String HelloWorld(){
         return "Hello World!!";
     }
-    //can also return html here
-    //the file will leave in resources?
 }
