9b3926dbb10a22a657b68a608925b6b79ac5af37
madinabolat <madinabolat01@gmail.com>
2025-10-22 20:50:09 -0400

    add unit test for new user register

--- systemize/src/main/java/com/example/systemize/dto/UserRegistrationDto.java
+++ systemize/src/main/java/com/example/systemize/dto/UserRegistrationDto.java
@@ -4,6 +4,11 @@ public class UserRegistrationDto {
     private String username;
     private String password;
 
+    public UserRegistrationDto(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
--- systemize/src/test/java/com/example/systemize/service/UserServiceImplTest.java
+++ systemize/src/test/java/com/example/systemize/service/UserServiceImplTest.java
@@ -1,20 +1,50 @@
 package com.example.systemize.service;
 
+import com.example.systemize.dto.UserRegistrationDto;
+import com.example.systemize.exception.userAlreadyExistsException;
+import com.example.systemize.model.User;
+import com.example.systemize.respository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UserServiceImplTest {
+    UserRepository userRepository = mock(UserRepository.class);
+    PasswordEncoder passwordEncoder = mock(PasswordEncoder.class);
+
     @Test
-    public void UserServiceImpl(){
+    public void ShouldRegisterNewUserSuccessfully(){
+        UserRegistrationDto userData = new UserRegistrationDto("madina", "123");
 
+        when(userRepository.findByUsername(userData.getUsername())).thenReturn(null);
+        when(passwordEncoder.encode(userData.getPassword())).thenReturn("123encoded");
+        //add invocation? read more
+        UserServiceImpl userService = new UserServiceImpl(userRepository, passwordEncoder);
 
+        User savedUser = userService.registerUser(userData); //this is returning null - investigate
+        assertEquals("madina", savedUser.getUsername());
+        assertEquals("123encoded", savedUser.getPasswordHash());
+    }
 
-
-//        registerUser(UserRegistrationDto userData):
-//        Test successful registration and handling of existing usernames.
-//        findByUsername(String username):
-//        Test finding existing users and handling non-existent users.
-//            Use Mockito to mock UserRepository and PasswordEncoder.
-
+    @Test
+    public void TestRegisterExistingUsername(){
 
     }
+
+    @Test
+    public void TestFindExistingUser(){
+
+    }
+
+    @Test
+    public void TestFindNonexistentUser(){
+
+    }
+
+
+
+
 }
