866c8592a719cf0e5f06897f413000163e00c229
madinabolat <madinabolat01@gmail.com>
2025-11-09 19:52:34 -0500

    test

--- systemize/src/test/java/com/example/systemize/service/AuthControllerTest.java
+++ systemize/src/test/java/com/example/systemize/service/AuthControllerTest.java
@@ -3,22 +3,31 @@ package com.example.systemize.service;
 import com.example.systemize.controller.AuthController;
 import com.example.systemize.dto.UserRegistrationDto;
 import com.example.systemize.model.User;
-import com.example.systemize.respository.UserRepository;
+import static org.mockito.Mockito.mock;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AuthControllerTest {
+    MockMvc mockMvc;
+    UserServiceImpl userService  = mock(UserServiceImpl.class);
+    AuthController authController;
 
+    @BeforeEach
+    public void test() {
+        this.authController = new AuthController(userService);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+    }
 
 //    POST /api/auth/register: Test successful registration and invalid input.
 //    POST /api/auth/login: Test successful login, incorrect credentials, and non-existent usernames.
@@ -31,19 +40,22 @@ public class AuthControllerTest {
 //                .body(userServiceImpl.registerUser(userData));
 //    }
 
-
-    UserServiceImpl userService  = mock(UserServiceImpl.class);
-    AuthController authController;
-
     @Test
-    public void registersNewUserSuccessfully(){
+    public void registersNewUserSuccessfully() throws JsonProcessingException {
         UserRegistrationDto userData = new UserRegistrationDto("madina", "123");
-        User newUser = new User(); //do i need proper user with password?
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userDataJson = objectMapper.writeValueAsString(userData);
+        User newUser = new User();
+        newUser.setUsername("madina");
+        newUser.setPasswordHash("123hash");
         when(userService.registerUser(userData)).thenReturn(newUser);
-        authController.register(userData)
 
-
+        mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userDataJson) // why this content is errored out?
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())// no HttpStatus.Created. just status.isOk?
+                .andExpect(model().attributeHasErrors(newUser)); //  .body(userServiceImpl.registerUser(userData)); -> does it return user? or just body?
     }
 
 
